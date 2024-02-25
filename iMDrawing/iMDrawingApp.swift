66ca86e0b41ee5b76ce8328c079fbcbab172c0f4
Min Pyae <minmp2002@gmail.com>
2024-02-25 07:49:03 +0000

    Initial Commit

---
+++ iMDrawing/iMDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMDrawingApp.swift
+//  iMDrawing
+//
+//  Created by Chris Min on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iMDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
